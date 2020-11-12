350d799fdd1f4289191137f458d8ea5d2db08f5a
Gil Bernard <gbernard.psy@gmail.com>
2020-11-11 23:55:22 -0500

    hosplot addition

--- nov11.R
+++ nov11.R
@@ -11,6 +11,20 @@ list.files("/Users/gilbernard/Desktop/R files/ma covid/covid-19-dashboard_11-11-
 
 #read in files
 cases<- read.csv("/Users/gilbernard/Desktop/R files/ma covid/covid-19-dashboard_11-11-2020/Cases.csv")
+hospitalizations<- read_xlsx("/Users/gilbernard/Desktop/R files/ma covid/covid-19-dashboard_11-11-2020/Hospitalization from Hospitals.xlsx")
+deathsreported<- read.csv("/Users/gilbernard/Desktop/R files/ma covid/covid-19-dashboard_11-11-2020/DeathsReported.csv")
 
-view(cases)
+#hospitalizations
+hos<- hospitalizations%>%
+    rename("total_in_hospital"= `Total number of confirmed COVID patients in hospital today`, "net new"= `Net new number of confirmed COVID patients in hospital today`)%>%
+    filter(as.Date(Date)>"2020-06-01")
 
+attach(hos)
+
+ggplot(hos, aes(Date))+
+    geom_line(aes(y=total_in_hospital))+
+    geom_line(aes(y=`Confirmed ICU`), color="red")+
+    theme_classic()+
+    xlab(label = "Date")+
+    ylab(element_blank())+
+    labs(title = "hospitalizations and ICU")
